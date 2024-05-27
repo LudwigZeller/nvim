1b7e931373350dba3823f8554701e594bcc4e79d
LudwigZeller <91737401+LudwigZeller@users.noreply.github.com>
2024-05-27 22:43:11 +0200

    First Flake

--- flake.nix
+++ flake.nix
@@ -8,14 +8,37 @@
     inputs.flake-utils.inputs.follows = "nixpkgs";
   };
 
-  outputs = { self, lib, stdenv, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
-    name="nvim-personal";
-    src = ./;
+  outputs = { self, stdenv, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
     formatter.${system} = pkgs.alejandra;
     packages.${system}.default = stdenv.mkDerivation {
+      name="nvim-personal";
+      src = ./.;
+      BuildInputs = with pkgs;[
+        neovim
+        neovide
+        php83
+        php83Packages.composer
+        nodejs
+        jdk
+        julia
+        python312Packages.python
+        python312Packages.pip
+        lua54Packages.lua
+        lua54Packages.luarocks
+        cargo
+        rustc
+      ];
 
+      installPhase = ''
+        mkdir -p $out/run
+        cp ./lazy-lock.json $out/run/lazy-lock.json
+        cp ./init.lua $out/run/init.lua
+        cp -r ./lua $out/run/lua
+        ln $XDG_CONFIG_DIR/nvim $out/run
+
+        mkdir -p $out/bin
+        cp ${pkgs.neovim}/bin/nvim $out/bin
+      '';
     };
-
-
   });
 }
