807ac1e55cc42f477bceda485be574202cd2025e
LudwigZeller <91737401+LudwigZeller@users.noreply.github.com>
2024-07-01 20:33:34 +0200

    WIP flake.nix

--- flake.nix
+++ flake.nix
@@ -2,46 +2,16 @@
   description = "Neovim Flake for my personilized setup";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
-    formatter.${system} = pkgs.alejandra;
-    packages.default = pkgs.stdenv.mkDerivation {
-      name="nvim";
-      src = ./.;
-      nativeBuildInputs = with pkgs; [
-        makeWrapper
-      ];
-      BuildInputs = with pkgs;[
-        neovide
-        php83
-        php83Packages.composer
-        nodejs
-        jdk
-        julia
-        python312Packages.python
-        python312Packages.pip
-        lua54Packages.lua
-        lua54Packages.luarocks
-        cargo
-        rustc
-      ];
-
-      installPhase = ''
-        mkdir -p $out/run
-        cp ./lazy-lock.json $out/run
-        cp ./init.lua $out/run
-        cp -r ./lua $out/run
-        # ln $XDG_CONFIG_HOME/nvim $out/run
-
-        mkdir -p $out/bin
-        wrapProgram ${pkgs.neovim}/bin/nvim $out/bin/nvim --set VIMINIT "source $out/run/init.lua"
-        # cp ${pkgs.neovim}/bin/nvim $out/bin
-        wrapProgram ${pkgs.neovide}/bin/neovide $out/bin/neovide --set NEOVIM_BIN "${pkgs.neovim}/bin/nvim"
-        # cp ${pkgs.neovide}/bin/neovide $out/bin
-      '';
+  outputs = { self, nixpkgs, flake-utils, neovim }: flake-utils.lib.eachDefaultSystem (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
+    formatter = pkgs.alejandra;
+    packages.default = neovim.packages.${system}.neovim;
+    apps.default = {
+      type="app";
+      program = "${neovim.packages.${system}.neovim}/bin/nvim}";
     };
   });
 }
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -6,6 +6,12 @@ return {
 
   { 'ThePrimeagen/vim-be-good', },
 
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {}
+  },
+
   --[[ DevDocs ]]
   {
     "luckasRanarison/nvim-devdocs",
@@ -40,31 +46,31 @@ return {
   --[[ Flash ]]
   {
     "folke/flash.nvim",
-    opts = { modes = { char = { enabled = false } } },
-    -- keys = {
-    --   { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash", },
-    --   { "F", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter", },
-    --   { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash", },
-    --   {
-    --     "R",
-    --     mode = { "o", "x" },
-    --     function() require("flash").treesitter_search() end,
-    --     desc = "Treesitter Search",
-    --   },
-    --   {
-    --     "<c-s>",
-    --     mode = { "c" },
-    --     function() require("flash").toggle() end,
-    --     desc = "Toggle Flash Search",
-    --   },
-    -- },
+    opts = { modes = { char = { enabled = true, jump_labels = true } } },
     keys = {
-      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+      { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash", },
+      { "F", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter", },
+      { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash", },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc = "Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc = "Toggle Flash Search",
+      },
     },
+    -- keys = {
+    --   { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    --   { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    --   { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+    --   { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    --   { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    -- },
   },
 
   --[[ Nvim Surround ]]
