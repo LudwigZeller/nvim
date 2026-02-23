9217c205a3eebf1dfd88bc780f6a484c58f5c1ba
Ludwig Zeller <91737401+LudwigZeller@users.noreply.github.com>
2026-02-24 00:50:31 +0100

    farthen-dur manual

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixvim.url = "github:nix-community/nixvim";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
   outputs =
-    { nixvim, flake-parts, ... }@inputs:
+    { nixpkgs, nixvim, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -16,28 +17,30 @@
         "aarch64-darwin"
       ];
 
-      perSystem =
-        { system, ... }:
-        let
-          nixvimLib = nixvim.lib.${system};
-          nixvim' = nixvim.legacyPackages.${system};
-          nixvimModule = {
-            inherit system;
-            module = import ./config;
-            extraSpecialArgs = {
-              # inherit (inputs) foo;
-            };
-          };
-          nvim = nixvim'.makeNixvimWithModule nixvimModule;
-        in
-        {
-          checks = {
-            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-          };
-
-          packages = {
-            default = nvim;
+      perSystem = { system, ... }:
+      let
+        nixvimLib = nixvim.lib.${system};
+        nixvim' = nixvim.legacyPackages.${system};
+        nixvimModule = {
+          inherit system;
+          module = import ./config;
+          extraSpecialArgs = {
           };
         };
+        nvim = nixvim'.makeNixvimWithModule nixvimModule;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        checks = {
+          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+        };
+
+        packages = {
+          default = nvim;
+          neovide = pkgs.writeShellScriptBin "neovide" ''
+              export PATH="${pkgs.lib.makeBinPath [ pkgs.neovide ]}:$PATH"
+              exec neovide --neovim ${nvim}/bin/nvim "$@"
+            '';
+        };
+      };
     };
-    }
+}
